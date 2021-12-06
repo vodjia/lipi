a63e155a447675371d3c4cbc9152ec4cdf4195b7
Jiacheng Huang <huangjiacheng0709@outlook.com>
2021-12-05 23:04:46 -0600

    Use new LiFiReceiver in receive.py

--- receiver.py
+++ receiver.py
@@ -22,25 +22,32 @@ class LiFiReceiver:
         packet_middle = packet_range / 2
         high_threshold = packet_middle + 0.4 * packet_range
         low_threshold = packet_middle - 0.4 * packet_range
-        print(high_threshold)
-        print(low_threshold)
         for i in range(len(packet)):
             if packet[i] < low_threshold:
                 packet[i] = 0
             elif packet[i] > high_threshold:
                 packet[i] = 1
+            else:
+                packet[i] = 0
 
-    def listen(self):
-        data = []
+    def listen_raw(self):
+        raw_data = []
         while True:
             packet = self.listen_packet()
             self.process_packet(packet)
-            data.append(packet)
+            raw_data.append(packet)
 
             if packet.count(0) == len(packet):
                 break
-        return data
+        return raw_data
 
-receiver = LiFiReceiver()
+    def process_raw(self, raw_data):
+        raw_data = ''.join(str(i) for i in raw_data)
+        start = raw_data.find('111111111111111111000000000000000000000000000')
+        data = ''
+        for i in range(start + int(self.period / 2), len(raw_data), self.period):
+            data += raw_data[i]
+        return data[5:data.find('00000')]
 
-print(receiver.listen())
+    def listen(self):
+        return self.process_raw(self.listen_raw())
