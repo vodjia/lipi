a159d0e0b3a7fe157eb32200b64ff0be55ad684a
Jiacheng Huang <huangjiacheng0709@outlook.com>
2021-12-10 19:47:46 -0600

    Debugging receiver

--- receive.py
+++ receive.py
@@ -1,34 +1,33 @@
-from decode import decode4b5b
 from adc0820 import ADC0820
-import time
+from decode import decode4b5b
+from receiver import Receiver
 
-period = 1000000
-ns_in_s = 1000000000
+period = 10000000
+start_pattern = '11000'
 
-upper = 120
+total_count = 0
+error_count = 0
 
-receiver = ADC0820(
+device = ADC0820(
     db0=17, db1=27, db2=22, db3=23,
     db4=24, db5=25, db6=5, db7=6,
     rd_n=18, int_n=19, cs_n=20, ofl_n=21,
     wr_n=16, mode=26
 )
 
-listening = False
-packet = ''
+receiver = Receiver(device, period, 130)
 
 while True:
-    time.sleep((period - time.time_ns() % period) / ns_in_s)
-    raw = receiver.read()
-    if raw >= upper:
-        bit = '1'
-    else:
-        bit = '0'
-    if bit == '1' and len(packet) == 0:
-        listening = True
-    if listening:
-        packet += bit
-    if len(packet) % 5 == 0 and packet[-5:-1] == '01101':
-        listening = False
-        print(packet)
-        break
+    if receiver.receive_bit() == start_pattern[0]:
+        if receiver.receive_bits(len(start_pattern) - 1) == start_pattern[1:]:
+            total_count += 1
+            raw = receiver.receive()
+            data = decode4b5b(raw)
+            if data != b'Hello World!\n':
+                error_count += 1
+                print(raw)
+                print(data)
+            print(raw)
+            print('Total count: ' + str(total_count))
+            print('Error count: ' + str(error_count))
+            print('Error rate: ' + str(error_count / total_count))
--- receiver.py
+++ receiver.py
@@ -1,38 +1,32 @@
+import time
+
 class Receiver:
-    def __init__(self, port, packet_capacity=256, bits_per_packet=16,
-                 period=9, start_pattern='00110', end_pattern='01101',
-                 halt_pattern='00100'):
-        self.port = port
-        self.packet_capacity = packet_capacity
-        self.bits_per_packet = bits_per_packet
+    def __init__(self, device, period, threshold,
+                 start_pattern='11000', end_pattern='01101'):
+        self.device = device
         self.period = period
+        self.threshold = threshold
         self.start_pattern = start_pattern
         self.end_pattern = end_pattern
-        self.halt_pattern = halt_pattern
 
-    def listen_raw_packet(self):
-        packet = []
-        if self.port.is_open:
-            for _ in range(self.packet_capacity):
-                signal = self.port.read()
-                packet.append(int.from_bytes(signal, "little"))
-        return packet
+    def receive_bit(self):
+        ns_in_s = 1000000000
+        delta_ns = 1.05 * self.period - time.time_ns() % self.period
+        time.sleep(delta_ns / ns_in_s)
+        if self.device.read() >= self.threshold:
+            return '1'
+        return '0'
 
-    def process_raw_packet(self, packet):
-        data = ''
-        start = int(0.75 * self.period)
-        for i in range(start, len(packet), self.period):
-            data += str(packet[i])
-        begin = data.find(self.start_pattern) + len(self.start_pattern)
-        end = begin + self.bits_per_packet
-        return data[begin:end]
+    def receive_bits(self, count):
+        bits = ''
+        for i in range(count):
+            bits += self.receive_bit()
+        return bits
 
-    def listen(self):
-        data = ''
+    def receive(self):
+        bits = ''
         while True:
-            packet = self.process_raw_packet(self.listen_raw_packet())
-            print(packet)
-            data += packet
-            if packet[: len(self.halt_pattern)] == self.halt_pattern:
-                break
-        return data[: data.rfind(self.end_pattern)]
+            pattern = self.receive_bits(len(self.start_pattern))
+            if pattern == self.end_pattern:
+                return bits
+            bits += pattern
