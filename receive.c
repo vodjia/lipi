3b6ab9fe8e497e66749eba36bcd7719ee7d7b360
Jiacheng Huang <huangjiacheng0709@outlook.com>
2021-12-11 18:16:26 -0600

    Receive a packet

--- receive.c
+++ receive.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include "adc0820.h"
 
@@ -8,24 +10,27 @@ struct lifi_receiver {
 	unsigned char (*read)(void *);
 	unsigned long period;
 	unsigned long threshold;
-	unsigned char start_pattern;
-	unsigned char end_pattern;
+	size_t code_length;
+	char start_code;
+	char end_code;
 };
 
 struct lifi_receiver *lifi_receiver_new(void *device,
 					void *read,
 					unsigned long period,
 					unsigned long threshold,
-					unsigned char start_pattern,
-					unsigned char end_pattern)
+					size_t code_length,
+					char start_code,
+					char end_code)
 {
 	struct lifi_receiver *receiver = malloc(sizeof(*receiver));
 	receiver->device = device;
 	receiver->read = read;
 	receiver->period = period;
 	receiver->threshold = threshold;
-	receiver->start_pattern = start_pattern;
-	receiver->end_pattern = end_pattern;
+	receiver->code_length = code_length;
+	receiver->start_code = start_code;
+	receiver->end_code = end_code;
 	return receiver;
 }
 
@@ -34,12 +39,39 @@ void lifi_receiver_delete(struct lifi_receiver *receiver)
 	free(receiver);
 }
 
-unsigned char lifi_receive_bit(struct lifi_receiver *receiver)
+char lifi_receive_bit(struct lifi_receiver *receiver)
 {
+	struct timeval current;
+	gettimeofday(&current, NULL);
+	usleep(receiver->period - current.tv_usec % receiver->period);
 	if (receiver->read(receiver->device) > receiver->threshold)
 		return 1;
 	return 0;
 }
+
+char lifi_receive_bits(struct lifi_receiver *receiver, size_t count)
+{
+	char code = 0;
+	for (size_t i = 0; i < count; ++i) {
+		code <<= 1;
+		code |= lifi_receive_bit(receiver);
+	}
+	return code;
+}
+
+int lifi_receive(struct lifi_receiver *receiver, char *str, size_t count)
+{
+	size_t i = 0;
+	while (i < count) {
+		char code = lifi_receive_bits(receiver, receiver->code_length);
+		if (code == receiver->end_code)
+			return i;
+		str[i++] = code;
+	}
+	return -1;
+}
+
+#define BUFFER_CAPACITY 8192
 
 int main(void)
 {
@@ -53,17 +85,33 @@ int main(void)
 	struct adc0820_device *device = adc0820_new(chip, &pinout);
 	const unsigned long period = 1000;
 	const unsigned long threshold = 140;
-	const unsigned char start_pattern = 0x18;
-	const unsigned char end_pattern = 0x0d;
+	const size_t code_length = 5;
+	const char start_code = 0x18;
+	const char end_code = 0x0d;
 	struct lifi_receiver *receiver = lifi_receiver_new(device,
 							   adc0820_read,
 							   period,
 							   threshold,
-							   start_pattern,
-							   end_pattern);
-	for (int i = 0; ; ++i) {
-		printf("%d\n", lifi_receive_bit(receiver));
-		usleep(1000);
+							   code_length,
+							   start_code,
+							   end_code);
+	char buffer[BUFFER_CAPACITY] = { 0 };
+	for (;;) {
+		char code = lifi_receive_bit(receiver);
+		if (code == 1) {
+			code <<= code_length - 1;
+			code |= lifi_receive_bits(receiver, code_length - 1);
+			if (code == receiver->start_code) {
+				size_t length = 
+					lifi_receive(receiver,
+						     buffer,
+						     BUFFER_CAPACITY);
+				putchar('[');
+				for (size_t i = 0; i < length; ++i)
+					printf("%x, ", buffer[i]);
+				printf("]\n");
+			}
+		}
 	}
 	lifi_receiver_delete(receiver);
 	adc0820_delete(device);
