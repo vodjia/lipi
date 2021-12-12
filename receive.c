bba53685d9f811164cf7c7ff0a14d28aedadd3ae
Jiacheng Huang <huangjiacheng0709@outlook.com>
2021-12-11 19:52:28 -0600

    Update receive.c

--- receive.c
+++ receive.c
@@ -16,8 +16,8 @@ int main(void)
 		.db4 = 24, .db5 = 25, .db6 = 5, .db7 = 6, .ofl_n = 21
 	};
 	struct adc0820_device *device = adc0820_new(chip, &pinout);
-	const unsigned long period = 10000;
-	const unsigned long threshold = 140;
+	const unsigned long period = 1000;
+	const unsigned long threshold = 120;
 	const size_t code_length = 5;
 	const char start_code = 0x18;
 	const char end_code = 0x0D;
@@ -28,14 +28,13 @@ int main(void)
 							   code_length,
 							   start_code,
 							   end_code);
-	char raw[BUFFER_CAPACITY] = { 0 };
-	char data[BUFFER_CAPACITY] = { 0 };
 	for (;;) {
 		char code = lifi_receive_bit(receiver);
 		if (code == 1) {
 			code <<= code_length - 1;
 			code |= lifi_receive_bits(receiver, code_length - 1);
 			if (code == receiver->start_code) {
+				char raw[BUFFER_CAPACITY] = { 0 };
 				size_t length = 
 					lifi_receive(receiver,
 						     raw,
@@ -44,9 +43,10 @@ int main(void)
 				for (size_t i = 0; i < length; ++i)
 					printf("%x, ", raw[i]);
 				printf("]\n");
-				int d = decode4b5b(data, raw);
-				if (d < 0)
-					printf("Error at index: %d\n", d);
+				char data[BUFFER_CAPACITY] = { 0 };
+				int ret = decode4b5b(data, raw);
+				if (ret < 0)
+					printf("Error at index: %d\n", -ret);
 				else
 					printf("Data: %s\n", data);
 			}
