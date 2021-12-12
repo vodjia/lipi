bc3d101114117569595e866416bffeed01c768f9
Jiacheng Huang <huangjiacheng0709@outlook.com>
2021-12-11 18:24:13 -0600

    Add function declarations in receiver.h

--- receiver.h
+++ receiver.h
@@ -9,3 +9,19 @@ struct lifi_receiver {
 	char start_code;
 	char end_code;
 };
+
+struct lifi_receiver *lifi_receiver_new(void *device,
+					void *read,
+					unsigned long period,
+					unsigned long threshold,
+					size_t code_length,
+					char start_code,
+					char end_code);
+
+void lifi_receiver_delete(struct lifi_receiver *receiver);
+
+char lifi_receive_bit(struct lifi_receiver *receiver);
+
+char lifi_receive_bits(struct lifi_receiver *receiver, size_t count);
+
+int lifi_receive(struct lifi_receiver *receiver, char *str, size_t count);
